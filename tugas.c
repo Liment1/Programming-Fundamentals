2b78298d0f5e06eaa9d09a8c2db8f395b9c8c693
Liment <69704267+Liment1@users.noreply.github.com>
2023-12-02 15:45:26 +0700

    Update tugas.c

--- tugas.c
+++ tugas.c
@@ -8,18 +8,58 @@ int harga_baju(char tipe[], char nama[]);
 
 int main()
 {   
-    char nama[50], tipe_penj[3], jenis[3], nom[7], kode_pemb[15];
+    char nama[50], tipe_penj[3], jenis[3], nom[7], kode_pemb[13];
     int  nota, banyak, jumlah_baju, jumlah_bayar, total;
     jumlah_bayar = 0;
 
     // Pengulangan sesai pembelian baju
     printf("banyak pembelian Baju : ");
     scanf("%d", &banyak);
+    getchar();
     printf("\n");
+
+    char kode_arr[banyak][15], nama_arr[banyak][20], tipe_penj_arr[banyak][15];
+    int harga_arr[banyak], jumlah_arr[banyak];
+
+    //loop sesuai kemauan user
+    for (int i=0; i < banyak; i++)
+    {
+        //input user
+        printf("kode %d:  ", i+1);
+        fgets(kode_pemb, 13, stdin); 
+        char* ptr = strchr(kode_pemb, '\n'); 
+        if (ptr) { 
+            *ptr = '\0'; 
+        } 
+        
+        sscanf(kode_pemb, "%[^:]:%[^:]:%s", tipe_penj, jenis, nom);
+        printf("Banyak baju : ");
+        scanf("%d", &jumlah_baju); 
+        getchar();
+
+        //function tipe, nama, harga, jumlah, total
+        char *tipe_baju = tipe(tipe_penj);
+        char *nama_baju = jenis_baju(jenis);
+        int harga = harga_baju(tipe_baju, nama_baju);
+
+        jumlah_bayar = harga*jumlah_baju;
+        total += jumlah_bayar;
+
+        //masukkan ke dalam array
+        strcpy(kode_arr[i], kode_pemb);
+        strcpy(tipe_penj_arr[i], tipe_baju);
+        strcpy(nama_arr[i], nama_baju);
+        harga_arr[i] = harga;
+        jumlah_arr[i] = jumlah_bayar;
+
+        // printf("%s %s %s %d %d\n", kode_arr[i], tipe_penj_arr[i], nama_arr[i], harga_arr[i], jumlah_arr[i]);
+        // printf("%s Kemeja %s %d %d %d\n\n", tipe_baju, nama_baju, harga, jumlah_bayar, total);
+    }
 
     // Template nota dan input
     printf("No. Nota\t: ");
     scanf("%d", &nota);
+    getchar();
     printf("Nama Pembeli\t: ");
     scanf("%s", &nama);
     getchar();
@@ -30,34 +70,16 @@ int main()
     // }
 
     printf("----------------------------------------------------------------------------------------------------------------\n");
-    printf("%-22s %-22s %-22s %-22s %-22s\n", "No. Artikel", "Nama", "Harga", "jumlah_bayar", "Sub.Total");
+    printf("%-25s %-25s %-25s %-25s %-25s\n", "No. Artikel", "Nama", "Harga", "jumlah_bayar", "Sub.Total");
     printf("----------------------------------------------------------------------------------------------------------------    \n");
-
-    for (int i=0; i < banyak; i++)
-    {
-        scanf("%[^:]:%[^:]:%[^\n] ", tipe_penj, jenis, nom);
-        getchar();
-        scanf("%d", &jumlah_baju);
-
-
-        // fgets(kode_pemb, 13 ,stdin);
-        // getchar();
-        // sscanf(kode_pemb, "%[^:]:%[^:]:%[^\n]", tipe_penj, jenis, nom); 
-
-
-        char *tipe_baju = tipe(tipe_penj);
-        char *nama_baju = jenis_baju(jenis);
-        int harga = harga_baju(tipe_baju, nama_baju);
-
-        jumlah_bayar = harga*jumlah_baju;
-        total = jumlah_bayar;
-
-        printf("%s Kemeja %s %d %d %d\n", tipe_baju, nama_baju, harga, jumlah_bayar, total);
-    }
-
-
-    
+ 
     //lanjutan dari nota (pembelian, bayar, dll)
+    for (int i = 0; i < banyak; i++)
+    {
+        printf("%-22s Kemeja %s %s %d %-22d\n", kode_arr[i], tipe_penj_arr[i], nama_arr[i], harga_arr[i], jumlah_arr[i]);
+        fflush(stdout);
+    }
+    printf("%d", total);
 
 }
 
